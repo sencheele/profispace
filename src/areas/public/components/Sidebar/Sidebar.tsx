5d46850c7d44a104f90c5d88f2e41304f987b7e7
sencheele <sencheele@gmail.com>
2026-09-10 10:33:34 +0400

    FEAT: implement sidebar

--- src/areas/public/components/Sidebar/Sidebar.tsx
+++ src/areas/public/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,58 @@
+import { NavLink } from 'react-router-dom'
 import styles from './Sidebar.module.scss'
+import { useState } from 'react'
 
 const Sidebar = () => {
+    const [isSidebarActive, setIsSidebarActive] = useState(false)
+
+    const links = [
+        {
+            to: 'home',
+            label: 'Главная',
+        },
+        {
+            to: 'projects',
+            label: 'Проекты',
+        },
+        {
+            to: 'about',
+            label: 'Обо мне',
+        },
+    ]
+
+    const handleToggleSidebar = () => {
+        setIsSidebarActive(prev => !prev)
+    }
+
     return (
-        <aside className={styles.sidebar}>
-            Сайдбар
+        <aside className={`${styles.sidebar} ${isSidebarActive ? 'is-active' : ''}`}>
+            <a
+                className={styles.sidebar__logo}
+                href='/'
+            >
+                Sencheele
+            </a>
+
+            <nav className={styles.sidebar__nav}>
+                {
+                    links.map(link => (
+                        <NavLink
+                            className={({ isActive }) =>
+                                `${styles.sidebar__link} ${isActive ? 'is-active' : ''}`}
+                            to={link.to}
+                            key={link.to}
+                        >
+                            {link.label}
+                        </NavLink>
+                    ))
+                }
+            </nav>
+
+            <button
+                className={styles.sidebar__toggle}
+                type='button'
+                onClick={handleToggleSidebar}
+            ></button>
         </aside>
     )
 }
